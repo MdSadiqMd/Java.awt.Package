11bffeeb7925754db8e24cb23bf6144301886e1f
Md.Sadiq <mohammadsadiq4950gmail.com>
2023-04-15 02:08:59 +0530

    Committed

--- videoawt.java
+++ videoawt.java
@@ -13,9 +13,11 @@ We can create an GUI application code from importing from Frame Class or Creatin
 public class videoawt  {
     public videoawt(){
         Frame frame=new Frame(" Hello World");
-        Button button=new Button("Click Me");
-        button.setActionCommand("open");
+        Button button=new Button("Click Me");//create a button object
+        button.setBounds(20,40,80,40);//set the size of the button
+        frame.add(button);//add button to the webapp
         frame.setSize(400,300); //this creates an outer frame of the web app
+        frame.setLayout(null);//if we not write this line then the elements we add will come as same size of whole layout
         frame.setVisible(true); //this tells to allow to display the output we are doing
         frame.addWindowListener(new WindowAdapter() { //select second option close
             @Override
